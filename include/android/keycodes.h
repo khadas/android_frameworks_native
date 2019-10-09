8a6c788228b5db56f553e57d93ffb809351cab8e
qiu.zeng <qiu.zeng@amlogic.com>
2019-10-09 11:16:31 +0800

    BT: add app button and number button [1/3]
    PD#SWPL-14855
    
    Change-Id: I1282aa05ed11edd715fb45bdb15caf4d901a434e
    Signed-off-by: qiu.zeng <qiu.zeng@amlogic.com>

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -769,8 +769,10 @@ enum {
     /** all apps */
     AKEYCODE_ALL_APPS = 284,
     /** refresh key */
-    AKEYCODE_REFRESH = 285
-
+    AKEYCODE_REFRESH = 285,
+    /** youtube */
+    AKEYCODE_YOUTUBE = 286,
+    
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
 };

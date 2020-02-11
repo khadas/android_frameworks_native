8bfccf4bfae0af2c7432015932ab35bdeb6e07b9
Jian Wang <jian.wang@amlogic.com>
2020-02-11 19:25:13 +0800

    av1: add dv with av1 support for mp4
    PD#SWPL-20356
    
    Problem:
    android p do not support dv with av1
    
    Solution:
    add dv with av1 support for mp4
    
    Verify:
    U212
    
    Change-Id: Id61a55c087a9cef8df56ac0b71e6231f8db4809a
    Signed-off-by: Jian Wang <jian.wang@amlogic.com>

--- headers/media_plugin/media/openmax/OMX_VideoExt.h
+++ headers/media_plugin/media/openmax/OMX_VideoExt.h
@@ -299,6 +299,7 @@ typedef enum OMX_VIDEO_DOLBYVISIONPROFILETYPE {
     OMX_VIDEO_DolbyVisionProfileDvheDtb = 0x80,
     OMX_VIDEO_DolbyVisionProfileDvheSt  = 0x100,
     OMX_VIDEO_DolbyVisionProfileDvavSe  = 0x200,
+    OMX_VIDEO_DolbyVisionProfileDvav110  = 0x400,
     OMX_VIDEO_DolbyVisionProfileMax     = 0x7FFFFFFF
 } OMX_VIDEO_DOLBYVISIONPROFILETYPE;
 

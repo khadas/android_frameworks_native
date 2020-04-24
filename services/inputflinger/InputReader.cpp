9bb093cd1e3fb7fc80c77dee9de506b609d68aaa
goenjoy <goenjoy@khadas.com>
2020-04-24 15:31:30 +0800

    mouse: Add right mouse button return function
    Change-Id: Idb4a158689e8772af2610f5f4ade2ef170483af0

--- services/inputflinger/InputReader.cpp
+++ services/inputflinger/InputReader.cpp
@@ -1375,13 +1375,7 @@ uint32_t CursorButtonAccumulator::getButtonState() const {
         result |= AMOTION_EVENT_BUTTON_PRIMARY;
     }
     if (mBtnRight) {
-        char targetProduct[PROPERTY_VALUE_MAX] = {0};
-        property_get("ro.target.product", targetProduct, "");
-        if (strcmp(targetProduct, "box") == 0 || strcmp(targetProduct, "atv") == 0) {
             result |= AMOTION_EVENT_BUTTON_BACK;
-        } else {
-            result |= AMOTION_EVENT_BUTTON_SECONDARY;
-        }
     }
     if (mBtnMiddle) {
         result |= AMOTION_EVENT_BUTTON_TERTIARY;

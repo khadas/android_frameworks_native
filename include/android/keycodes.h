2c7240c9a24a2891feb57e600ea3c39adf68de18
goenjoy <goenjoy@khadas.com>
2020-05-25 17:22:06 +0800

    IR remote: Fix IR remote key recognition KeyLayoutFile error [2/2]
    Change-Id: Iac78e99aee79cb9bc7f2ac1a44774b6af9292bd9

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -776,7 +776,12 @@ enum {
     AKEYCODE_THUMBS_DOWN = 287,
     /** Used to switch current account that is consuming content.
      * May be consumed by system to switch current viewer profile. */
-    AKEYCODE_PROFILE_SWITCH = 288
+    AKEYCODE_PROFILE_SWITCH = 288,
+    AKEYCODE_TV_KEYMOUSE_LEFT = 289,
+    AKEYCODE_TV_KEYMOUSE_RIGHT = 290,
+    AKEYCODE_TV_KEYMOUSE_UP = 291,
+    AKEYCODE_TV_KEYMOUSE_DOWN = 292,
+    AKEYCODE_TV_KEYMOUSE_MODE_SWITCH = 293
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.

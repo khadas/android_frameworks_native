TinkerBoard2-Android/frameworks-native

box:support for infrare simulate mouse.
Change-Id: I7224b873882de742b86b2dfda0dbaa298b34f2f7

services/inputflinger/reader/include/EventHub.h
@@ -119,6 +119,9 @@ enum {
     /* The input device has a rotary encoder */
     INPUT_DEVICE_CLASS_ROTARY_ENCODER = 0x00001000,
 
+    /* The input device has a mouse. */
+    INPUT_DEVICE_CLASS_KEYMOUSE       = 0x00004000,
+
     /* The input device is virtual (not a real device, not part of UI configuration). */
     INPUT_DEVICE_CLASS_VIRTUAL = 0x40000000,
 

khadas/android_frameworks_native

services/inputflinger/reader/include/EventHub.h
@@ -136,6 +136,9 @@ enum class InputDeviceClass : uint32_t {
     /* The input device has sysfs controllable lights */
     LIGHT = 0x00008000,
 
+    /* The input device has a mouse. */
+    INPUT_DEVICE_CLASS_KEYMOUSE       = 0x00010000,
+
     /* The input device is virtual (not a real device, not part of UI configuration). */
     VIRTUAL = 0x40000000,
 
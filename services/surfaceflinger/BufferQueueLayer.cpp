littleGnAl/mirror_platform_frameworks_native

SurfaceFlinger: update sideband hnd when layer queued buffer. [1/1]
PD#39639

Problem:
Sideband cann't displayed blank frame when layer disconnect

Solution:
update sideband when new frame set

Verify:
sc2

Change-Id: I30714a6f5e9bef8deae6937ea29111666f731459
Signed-off-by: baocheng sun <baocheng.sun@amlogic.com>

services/surfaceflinger/BufferQueueLayer.cpp
@@ -208,8 +208,15 @@ bool BufferQueueLayer::getSidebandStreamChanged() const {
 }
 
 bool BufferQueueLayer::latchSidebandStream(bool& recomputeVisibleRegions) {
+    bool updateSidebandStream = false;
+    if (hasFrameUpdate() && mSidebandStream.get()) {
+        ALOGD("[%s] have both sidebandstream & buffer, need udpate sideband.", getDebugName());
+        updateSidebandStream = true;
+    }
+
     bool sidebandStreamChanged = true;
-    if (mSidebandStreamChanged.compare_exchange_strong(sidebandStreamChanged, false)) {
+    if (mSidebandStreamChanged.compare_exchange_strong(sidebandStreamChanged, false)
+        || updateSidebandStream) {
         // mSidebandStreamChanged was changed to false
         mSidebandStream = mConsumer->getSidebandStream();
         auto* layerCompositionState = editCompositionState();

khadas/android_frameworks_native

BUG=176557125

services/surfaceflinger/BufferQueueLayer.cpp
@@ -208,8 +208,15 @@ bool BufferQueueLayer::getSidebandStreamChanged() const {
 }
 
 bool BufferQueueLayer::latchSidebandStream(bool& recomputeVisibleRegions) {
+    bool updateSidebandStream = false;
+    if (hasFrameUpdate() && mSidebandStream.get()) {
+        ALOGD("[%s] have both sidebandstream & buffer, need update sideband.", getDebugName());
+        updateSidebandStream = true;
+    }
+
     bool sidebandStreamChanged = true;
-    if (mSidebandStreamChanged.compare_exchange_strong(sidebandStreamChanged, false)) {
+    if (mSidebandStreamChanged.compare_exchange_strong(sidebandStreamChanged, false)
+        || updateSidebandStream) {
         // mSidebandStreamChanged was changed to false
         mSidebandStream = mConsumer->getSidebandStream();
         auto* layerCompositionState = editCompositionState();
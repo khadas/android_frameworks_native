17cf003817ab7e18da0f2e02ee8cd7a72845045e
dzy <dzy@rock-chips.com>
2022-10-13 10:18:28 +0800

    multi-display:inputflinger support mouse display on external screen.
    Change-Id: I49346fbff900a44d0885b4c63c22e34cf43ddf38

--- services/inputflinger/reader/mapper/CursorInputMapper.cpp
+++ services/inputflinger/reader/mapper/CursorInputMapper.cpp
@@ -24,6 +24,7 @@
 #include "CursorScrollAccumulator.h"
 #include "PointerControllerInterface.h"
 #include "TouchCursorInputMapperCommon.h"
+#include <cutils/properties.h>
 
 namespace android {
 
@@ -267,6 +268,7 @@ void CursorInputMapper::dumpParameters(std::string& dump) {
 void CursorInputMapper::reset(nsecs_t when) {
     mButtonState = 0;
     mDownTime = 0;
+    mDisplayId=0;
 
     mPointerVelocityControl.reset();
     mWheelXVelocityControl.reset();
@@ -360,6 +362,22 @@ void CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
         pointerCoords.setAxisValue(AMOTION_EVENT_AXIS_RELATIVE_X, deltaX);
         pointerCoords.setAxisValue(AMOTION_EVENT_AXIS_RELATIVE_Y, deltaY);
         displayId = mPointerController->getDisplayId();
+
+	char mMousePresentation[PROPERTY_VALUE_MAX] = {0};
+        property_get("sys.mouse.presentation", mMousePresentation, "0");
+        if (strcmp(mMousePresentation, "1") == 0) {
+           //displayId = mDisplayId;
+           float minX, minY, maxX, maxY;
+           if (mPointerController->getBounds(&minX, &minY, &maxX, &maxY)) {
+               if(xCursorPosition==minX||xCursorPosition==maxX||yCursorPosition==minY||yCursorPosition==maxY){
+                   displayId=getPolicy()->notifyDisplayIdChanged();
+                   //mDisplayId=displayId;
+               }
+           }
+        }else{
+           displayId = mPointerController->getDisplayId();
+        }
+
     } else {
         // Pointer capture and navigation modes
         pointerCoords.setAxisValue(AMOTION_EVENT_AXIS_X, deltaX);
